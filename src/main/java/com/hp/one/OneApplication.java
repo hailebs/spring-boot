158e823e549d97f193c5be57d21102fbe82dfb8c
haile_b <haile.hb81@gmail.com>
2019-12-25 13:08:47 -0800

    can not move to another branch with out commiting changes

--- src/main/java/com/hp/one/OneApplication.java
+++ src/main/java/com/hp/one/OneApplication.java
@@ -3,7 +3,7 @@ package com.hp.one;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//
+//trial
 @SpringBootApplication
 public class OneApplication {
 
